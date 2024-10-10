cf8998269a39cfbe1f72fc05782dd8ff2dc283a3
Nimira43 <n.rabone72@aol.com>
2024-10-10 03:22:13 +0100

    Getting ready to put the theory into code

--- 01-Eraser-Pencil-Project/perceptron.js
+++ 01-Eraser-Pencil-Project/perceptron.js
@@ -7,6 +7,9 @@ const trainInputs = [
 ]
 
 const trainLabels = [1, 1, 0, 0, 0]
-const weights = [0.1, -0.3]
-const b = 0.5 // bias
+const weights = [0.1, -0.3] // old weights
+const weights = [0.3, 0.4] // new adjusted weights
+const b = 0.5 // old bias
+const b = 0.6 // new adjusted bias
 const n = 0.1 // learning rate
+
